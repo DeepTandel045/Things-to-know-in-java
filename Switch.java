bd7d722255969d86a0caa3eaa2a0c74a779806ad
DeepTandel045 <tandeldeep6106@gmail.com>
2025-08-16 16:04:23 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();   // input day number
+
+        switch(day) {
+            case 1: System.out.print("Monday"); break;
+            case 2: System.out.print("Tuesday"); break;
+            case 3: System.out.print("Wednesday"); break;
+            case 4: System.out.print("Thursday"); break;
+            case 5: System.out.print("Friday"); break;
+            case 6: System.out.print("Saturday"); break;
+            case 7: System.out.print("Sunday"); break;
+            default: System.out.print("Invalid");
+        }
+    }
+}
